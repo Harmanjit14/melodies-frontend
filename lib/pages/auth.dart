cc44785f35314158374f329ea5be0f7d7e2275cc
Harmanjit Singh <harmanjit140500@gmail.com>
2020-12-10 05:55:13 +0530

    auth ++

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -1,6 +1,8 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Authentication extends StatefulWidget {
   @override
@@ -10,8 +12,75 @@ class Authentication extends StatefulWidget {
 class _AuthenticationState extends State<Authentication> {
   Widget login() {
     return Container(
-      child: Image.asset("lib/assets/animation2.gif")
-    );
+        child: SingleChildScrollView(
+      child: Container(
+        child: Column(
+          children: [
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                Container(
+                  alignment: Alignment.center,
+                  height: MediaQuery.of(context).size.height * 0.4,
+                  child: Image.asset(
+                    "lib/assets/animation2.gif",
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Container(
+                  // alignment: Alignment.center,
+                  child: Text(
+                    "Login Here!",
+                    style: GoogleFonts.poppins(
+                        fontSize: 30,
+                        textStyle: TextStyle(color: Colors.white),
+                        shadows: <Shadow>[
+                          Shadow(
+                            offset: Offset(0.5, 0.5),
+                            blurRadius: 2,
+                            color: Colors.white,
+                          ),
+                        ]),
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                    focusColor: Colors.white,
+                    fillColor: Colors.white,
+                    filled: true,
+                    hintText: "Email",
+                    prefixIcon: Icon(
+                      FontAwesomeIcons.user,
+                    ),
+                    hintStyle: TextStyle(fontSize: 18),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: BorderSide.none)),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(20, 15, 20, 0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                    focusColor: Colors.white,
+                    fillColor: Colors.white,
+                    filled: true,
+                    hintText: "Password",
+                    prefixIcon: Icon(Icons.lock_outline),
+                    hintStyle: TextStyle(fontSize: 18),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: BorderSide.none)),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ));
   }
 
   Widget register() {}
