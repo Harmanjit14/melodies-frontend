3b905e78c2d491a529ee4f5c4144f7a93f5a00e3
Harmanjit Singh <harmanjit140500@gmail.com>
2021-01-08 02:52:44 +0530

    removed return wid

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -235,7 +235,7 @@ class _AuthenticationState extends State<Authentication> {
     ));
   }
 
-  Widget register() {}
+  // Widget register() {}
 
   @override
   Widget build(BuildContext context) {
